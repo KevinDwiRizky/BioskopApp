707b734ca040bc74e2338086f78e6bbb576d86e4
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2024-03-26 08:28:16 +0700

    new project

---
+++ src/main/java/com/kevin/bioskop/LivecodeBioskopApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.bioskop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecodeBioskopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivecodeBioskopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/bioskop/LivecodeBioskopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.bioskop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecodeBioskopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
