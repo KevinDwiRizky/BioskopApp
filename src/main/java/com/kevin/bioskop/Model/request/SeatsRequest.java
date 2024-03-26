7acd33b759ab3ba2e3c9f2cd139f2e4fd5f33aea
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2024-03-26 14:00:04 +0700

    update validation for dto

--- src/main/java/com/kevin/bioskop/Model/request/SeatsRequest.java
+++ src/main/java/com/kevin/bioskop/Model/request/SeatsRequest.java
@@ -1,6 +1,5 @@
 package com.kevin.bioskop.Model.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SeatsRequest {
-    @NotBlank(message = "Seat number must not be blank")
     private String seatNumber;
-    @NotBlank(message = "Theater ID must not be blank")
     private String theaterId;
+
 }
